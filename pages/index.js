2aa10d803e7b742a227930de60f6df0e64b9ffaf
kevinsims1 <kevindsims1@gmail.com>
2020-05-31 16:54:20 -0700

    tablet getting there

--- pages/index.js
+++ pages/index.js
@@ -49,101 +49,134 @@ export default function Home() {
           </div>
         </div>
       </header>
-
       <main>
-        <div className="section">
-          <h1>Weed should help your headache, not cause it</h1>
-          <p>
-            Help make getting weed hassle free. Get a professional website to
-            help with online orders, product updates and more.
-          </p>
-          <button className="getStartedBtn">GET STARTED</button>
-          <h4>Discounts Up To 30% When You Mention Covid</h4>
+        <div className="headSec">
+          <img src="/happy.png" className="happyTop" />
+          <div className="headSection">
+            <h1 className="mainHeader">
+              Weed should help your headache, not cause it
+            </h1>
+            <p>
+              Help make getting weed hassle free. Get a professional website to
+              help with online orders, product updates and more.
+            </p>
+            <button className="getStartedBtn">GET STARTED</button>
+            <h4>Discounts Up To 30% When You Mention Covid</h4>
+          </div>
           <img src="/happy.png" className="happy" />
         </div>
-        <div className="section">
-          <h1 className="why">Why Work With Us?</h1>
-          <img src="/money.png" className="money" id="sectionPic" />
-          <h1 className="sectionH">Increase Your Sales</h1>
-          <p>
-            Companies who chose Marys Site have seen increases in sales, up to
-            20%!
-          </p>
-          <h5>*Online sales have gone up 50% nationwide since Covid.</h5>
-        </div>
-        <div className="section">
-          <img src="/expert.png" className="expert" id="sectionPic" />
-          <h1 className="sectionH">Work With Experts</h1>
-          <p>
-            Work with a team of designers and enginners who are here to
-            collaborate with you in the proccess of bringing your vision to
-            life.
-          </p>
-        </div>
-        <div className="section">
-          <img src="/chart.png" className="expert" id="sectionPic" />
-          <h1 className="sectionH">Analyze Your Data</h1>
-          <p>
-            When you chose Marys site, you will be able to see what products are
-            doing best and worst. Making it easy to decide what to restock with.
-          </p>
+        <h1 className='wwwu'>Why Work With Us?</h1>
+        <div className="mainSection">
+          <div className="section">
+            <img src="/money.png" className="money" id="sectionPic" />
+            <h1 className="sectionH">Increase Your Sales</h1>
+            <p className='sectionD'>
+              Companies who chose Marys Site have seen increases in sales, up to
+              20%!
+            </p>
+            <h5>*Online sales have gone up 50% nationwide since Covid.</h5>
+          </div>
+          <div className="section">
+            <img src="/expert.png" className="expert" id="sectionPic" />
+            <h1 className="sectionH">Work With Experts</h1>
+            <p className='sectionD'>
+              Work with a team of designers and enginners who are here to
+              collaborate with you in the proccess of bringing your vision to
+              life.
+            </p>
+          </div>
+          <div className="section">
+            <img src="/chart.png" className="expert" id="sectionPic" />
+            <h1 className="sectionH">Analyze Your Data</h1>
+            <p className='sectionD'>
+              When you chose Marys site, you will be able to see what products
+              are doing best and worst. Making it easy to decide what to restock
+              with.
+            </p>
+          </div>
         </div>
         <div className="plans">
           <div className="plan">
-            <div className='planW'>
+            <div className="planW">
               <h2 className="planH">Redesigned Website</h2>
-              <h3 className='planP'>$1500</h3>
+              <h3 className="planP">$1500</h3>
               <button className="planBtn">CONTINUE</button>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Design Check</h4>
-              <p className='planD'>Our designers will review your current site and take notes of issues found.</p>
+            <div className="planM">
+              <h4 className="planMH">Design Check</h4>
+              <p className="planD">
+                Our designers will review your current site and take notes of
+                issues found.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Collaborate</h4>
-              <p className='planD'>We'll present these problems, as well as ideas on possible solutions. From there we'll collaborate to ensure we are capturing your culture, in our design.</p>
+            <div className="planM">
+              <h4 className="planMH">Collaborate</h4>
+              <p className="planD">
+                We'll present these problems, as well as ideas on possible
+                solutions. From there we'll collaborate to ensure we are
+                capturing your culture, in our design.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Implement</h4>
-              <p className='planD'>After we reach A final design our engineers will take over and implent the design.</p>
+            <div className="planM">
+              <h4 className="planMH">Implement</h4>
+              <p className="planD">
+                After we reach A final design our engineers will take over and
+                implent the design.
+              </p>
             </div>
           </div>
           <div className="plan">
-            <div className='planW'>
+            <div className="planW">
               <h2 className="planH">Order Automation</h2>
-              <h3 className='planP'>$3000</h3>
+              <h3 className="planP">$3000</h3>
               <button className="planBtn">CONTINUE</button>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Get Online Orders In Real Time</h4>
-              <p className='planD'>Our system will ensure you get your customers order the moment its placed.</p>
+            <div className="planM">
+              <h4 className="planMH">Get Online Orders In Real Time</h4>
+              <p className="planD">
+                Our system will ensure you get your customers order the moment
+                its placed.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Know Your Customers</h4>
-              <p className='planD'>With our order automation you will be able to keep track of customer data, such as what products are selling fast or h.</p>
+            <div className="planM">
+              <h4 className="planMH">Know Your Customers</h4>
+              <p className="planD">
+                With our order automation you will be able to keep track of
+                customer data, such as what products are selling fast or h.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Stay Updated</h4>
-              <p className='planD'>Along side the implementation, our team will make sure your software stays up to date with the latest technologies.</p>
+            <div className="planM">
+              <h4 className="planMH">Stay Updated</h4>
+              <p className="planD">
+                Along side the implementation, our team will make sure your
+                software stays up to date with the latest technologies.
+              </p>
             </div>
           </div>
           <div className="plan">
-            <div className='planW'>
+            <div className="planW">
               <h2 className="planH">Professional Web App</h2>
-              <h3 className='planP'>$6000</h3>
+              <h3 className="planP">$6000</h3>
               <button className="planBtn">CONTINUE</button>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Automated Order System</h4>
-              <p className='planD'>Get everything included in the "Order Automation" package.</p>
+            <div className="planM">
+              <h4 className="planMH">Automated Order System</h4>
+              <p className="planD">
+                Get everything included in the "Order Automation" package.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Tailored Design</h4>
-              <p className='planD'>Get everything included in the "Website Redesign" package.</p>
+            <div className="planM">
+              <h4 className="planMH">Tailored Design</h4>
+              <p className="planD">
+                Get everything included in the "Website Redesign" package.
+              </p>
             </div>
-            <div className='planM'>
-              <h4 className='planMH'>Easy Upload</h4>
-              <p className='planD'>We have created a system that makes adding, removing, updating current inventory as easy as a click of a button.</p>
+            <div className="planM">
+              <h4 className="planMH">Easy Upload</h4>
+              <p className="planD">
+                We have created a system that makes adding, removing, updating
+                current inventory as easy as a click of a button.
+              </p>
             </div>
           </div>
         </div>
@@ -159,6 +192,10 @@ export default function Home() {
           padding: 0;
           border: none;
           background-color: none;
+        }
+
+        .container{
+          margin: 0 1em
         }
 
         .getStartedBtn {
@@ -207,7 +244,20 @@ export default function Home() {
           align-items: center;
         }
 
-        .plans{
+        .headSec{
+          margin: 0em 5em;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .headSection {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .plans {
           margin: 1em 3em;
           display: flex;
           flex-direction: column;
@@ -238,40 +288,40 @@ export default function Home() {
         }
 
         .sectionH {
-          margin: 0.5em 1em 0.5em 1em;
+          margin: 0.5em 1em;
         }
 
-        .plan{
+        .plan {
           margin: 0 0 2em 0;
         }
 
-        .planH{
+        .planH {
           text-align: left;
           margin: 1em 0 0.3em 0;
           padding: 0;
         }
 
-        .planP{
+        .planP {
           margin: 0;
           padding: 0;
-          font-size: 34px
+          font-size: 34px;
         }
 
-        .planD{
+        .planD {
           text-align: left;
-          margin: .2em 0
+          margin: 0.2em 0;
         }
 
-        .planW{
-          border-bottom: 2px solid #5D675B;
-          color: black
+        .planW {
+          border-bottom: 2px solid #5d675b;
+          color: black;
         }
 
-        .planMH{
+        .planMH {
           font-size: 24px;
           font-weight: bold !important;
           margin: 1.5em 0 0em 0;
-          color: black
+          color: black;
         }
 
         p {
@@ -291,6 +341,12 @@ export default function Home() {
         }
 
         .happy {
+          height: 11em;
+          width: 11em;
+        }
+
+        .happyTop {
+          display: none;
           height: 11em;
           width: 11em;
         }
@@ -336,6 +392,19 @@ export default function Home() {
         }
 
         @media only screen and (min-width: 760px) {
+          .wwwu{
+            font-size: 40px;
+            margin: 0
+          }
+
+          .headSec{
+            margin: .5em 4em 8em 4em;
+            display: flex;
+            flex-direction: row;
+            justify-items: center !important
+          }
+
+
           .mobileNav {
             display: none;
           }
@@ -353,6 +422,50 @@ export default function Home() {
             z-index: 0 !important;
             background: none !important;
             position: relative !important;
+          }
+
+          .sectionH{
+            font-size: 20px;
+            margin: .5em 0
+          }
+
+          .section {
+            flex-direction: column;
+            margin: 0.5em 1em;
+            align-items: center;
+            min-width: 33.3%
+          }
+
+          .mainSection{
+            margin: .5em 3em 2.5em 3em;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly !important
+          }
+
+          .sectionD{
+            margin: 0;
+            font-size: 18px;
+          }
+
+          #sectionPic{
+            width: 8em;
+            height: 8em
+
+          }
+
+          .headSection {
+            margin: 0 0;
+            max-width: 50%;
+          }
+
+          .happyTop {
+            display: block !important;
+          }
+
+          .mainHeader {
+            margin: 1em 0 0.5em 0;
+            font-size: 35px;
           }
         }
 
@@ -380,7 +493,7 @@ export default function Home() {
           padding: 0;
           margin: 0;
           font-family: 'Roboto', sans-serif;
-          color: #5D675B
+          color: #5d675b;
         }
 
         * {
